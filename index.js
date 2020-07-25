31b67229121d215f5439ad7f2c5beab012bb1a5f
mecharan14 <methatha14@gmail.com>
2020-07-26 01:12:52 +0530

    Colors for any link test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const proxy = 'https://cors-anywhere.herokuapp.com/';
-const proxy = ""
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+// const proxy = ""
 
 const init = () =>{
     let cont = document.querySelector("#linkCon");
